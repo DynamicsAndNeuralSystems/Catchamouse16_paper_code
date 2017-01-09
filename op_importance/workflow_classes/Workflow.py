16cd20aa4a97c2af44ef7f248765d02f070d0d42
sarabsethi <sarab.sethi93@gmail.com>
2017-01-09 15:32:42 +0000

    Change input file paths

--- op_importance/workflow_classes/Workflow.py
+++ op_importance/workflow_classes/Workflow.py
@@ -12,7 +12,7 @@ import modules.feature_importance.PK_feat_array_proc as fap
 import matplotlib.pyplot as plt
 
 class Workflow:
-    
+
     def __init__(self,task_names,input_method,stats_method,redundancy_method,combine_tasks_method = 'mean',
                  combine_tasks_norm = None,
                  select_good_perf_ops_method = 'sort_asc',
@@ -25,13 +25,13 @@ class Workflow:
         task_names : list of str
             A list of task names to be included in this workflow
         input_method : Data_Input
-            The data input method used to read the data from disk. 
+            The data input method used to read the data from disk.
         stat_method : Feature_Stats
             The mehtod used to calculate the statistics
-        redundancy_method : Reducing_Redundancy   
+        redundancy_method : Reducing_Redundancy
             The method used to reduce the redundancy in the well performing features
         combine_tasks_method : str
-            The name describing the method used to combine the statistics for each task to create a single 1d arrray with 
+            The name describing the method used to combine the statistics for each task to create a single 1d arrray with
             a single entry for every operation
         combine_tasks_norm : str
             The name of the normalisation method applied to the stats of each task before the statistics for each task are combined
@@ -45,13 +45,13 @@ class Workflow:
         """
         self.task_names = task_names
         self.input_method = input_method
-        self.stats_method = stats_method        
+        self.stats_method = stats_method
         self.redundancy_method = redundancy_method
         self.combine_tasks_norm = combine_tasks_norm
         #normalise_array(data,axis,norm_type = 'zscore')
         if combine_tasks_method == 'mean':
             self.combine_tasks = self.combine_task_stats_mean
-            
+
         if combine_tasks_norm == 'zscore':
             self.combine_task_norm_method = lambda y : fap.normalise_masked_array(y,axis = 1,norm_type = 'zscore')[0]
         else:
@@ -61,27 +61,27 @@ class Workflow:
         if select_good_perf_ops_method == 'sort_asc':
             self.select_good_perf_ops = self.select_good_perf_ops_sort_asc
         self.select_good_perf_ops_norm = select_good_perf_ops_norm
-        
-        self.n_good_perf_ops = n_good_perf_ops 
+
+        self.n_good_perf_ops = n_good_perf_ops
         # -- list of Tasks for this workflow
         self.tasks = [Task.Task(task_name,self.input_method,self.stats_method) for task_name in task_names]
 
         # -- Counter array for number of problems calculated successfully for each operation
-        
-        # -- place holders 
+
+        # -- place holders
         self.good_op_ids = []
         self.stats_good_op = None
         self.stats_good_op_comb = None
         self.stats_good_perf_op_comb = None
         self.good_perf_op_ids = None
-                   
+
     def calculate_stats(self,is_keep_data = False):
         """
         Calculate the statistics of the features for each task using the method given by stats_method
         """
         for task in self.tasks:
-            task.calc_stats(is_keep_data = is_keep_data)     
-                      
+            task.calc_stats(is_keep_data = is_keep_data)
+
     def collect_stats_good_op_ids(self):
         """
         Collect all combined stats for each task and take stats for good operations only
@@ -97,7 +97,7 @@ class Workflow:
             else:
                 stats_good_op_ma_tmp = np.empty((self.good_op_ids.shape[0]))
             stats_good_op_ma_tmp[:] = np.NaN
-            
+
             ind = hlp.ismember(task.op_ids,self.good_op_ids,is_return_masked_array = True,return_dtype = int)
             # -- it is position in task.op_ids and i is position in self.good_op_ids
             for it,i in enumerate(ind):
@@ -106,23 +106,23 @@ class Workflow:
             # -- We return to the usual ordering: column equals feature
             stats_good_op_tmp.append(stats_good_op_ma_tmp.T)
         self.stats_good_op = np.ma.masked_invalid(np.vstack(stats_good_op_tmp))
-        
-        
+
+
     def combine_task_stats_mean(self):
         """
         Combine the stats of all the tasks using the average over all tasks
-        
+
         """
         self.stats_good_op_comb = self.combine_task_norm_method(self.stats_good_op).mean(axis=0)
-        
+
     def init_redundancy_method_problem_space(self):
         """
         Initialises the redundancy_method with the required parameters depending on the redundancy_method.compare_space
         """
         if self.redundancy_method.compare_space == 'problem_stats':
             self.redundancy_method.set_parameters(self.stats_good_op,self.good_op_ids,self.good_perf_op_ids)
-        
-           
+
+
     def find_good_op_ids(self, threshold):
         """
         Find the features that have been successfully calculated for more then threshold problems.
@@ -130,7 +130,7 @@ class Workflow:
         -----------
         threshold : int
             Only keep features that have been calculated for at least threshold tasks.
-        
+
         """
         # -- List of all op_ids for each task (with duplicates)
         op_ids_tasks = [item for task in self.tasks for item in task.op_ids.tolist()]
@@ -138,10 +138,10 @@ class Workflow:
         c = collections.Counter(op_ids_tasks)
         for key in c.keys():
             if c[key] >= threshold:
-                self.good_op_ids.append(key) 
-        self.good_op_ids = np.array(self.good_op_ids) 
-                                  
-    def load_task_attribute(self,attribute_name,in_path_pattern): 
+                self.good_op_ids.append(key)
+        self.good_op_ids = np.array(self.good_op_ids)
+
+    def load_task_attribute(self,attribute_name,in_path_pattern):
         """
         Load an attribute for all tasks from separate files
         Parameters:
@@ -150,21 +150,21 @@ class Workflow:
             The name of the attribute of Task to be saved
         out_path-pattern : string
             A string containing the pattern for the path pointing to the input file. Formatted as in_path_pattern.format(self.name,attribute_name)
-        """      
+        """
         for task in self.tasks:
             task.load_attribute(attribute_name,in_path_pattern)
-    
-    def read_data(self,is_read_feature_data = True): 
+
+    def read_data(self,is_read_feature_data = True):
         """
-        Read the data for all tasks from disk using the method given by self.input_method 
+        Read the data for all tasks from disk using the method given by self.input_method
         Parameters:
         -----------
         is_read_feature_data : bool
             Is the feature data to be read
-        """   
+        """
         for task in self.tasks:
-            task.read_data(is_read_feature_data = is_read_feature_data)           
-    
+            task.read_data(is_read_feature_data = is_read_feature_data)
+
     def save_task_attribute(self,attribute_name,out_path_pattern):
         """
         Save an attribute of of all tasks to separate files
@@ -174,54 +174,55 @@ class Workflow:
             The name of the attribute of Task to be saved
         out_path-pattern : string
             A string containing the pattern for the path pointing to the output file. Formatted as out_path_pattern.format(self.name,attribute_name)
-        """ 
+        """
         for task in self.tasks:
             task.save_attribute(attribute_name,out_path_pattern)
-            
+
     def select_good_perf_ops_sort_asc(self):
         """
         Select a subset of well performing operations
         """
-                
+
         if self.select_good_perf_ops_norm in ['z-score','zscore'] :
             all_classes_good_norm = fap.normalise_masked_array(self.stats_good_op,axis = 1,norm_type = 'zscore')[0]
-        
+
         elif self.select_good_perf_ops_norm == 'mean-norm':
             all_classes_good_mean = np.ma.masked_invalid(np.ma.mean(self.stats_good_op,axis = 1))
-            all_classes_good_norm = (self.stats_good_op.T / all_classes_good_mean).T  
-        
+            all_classes_good_norm = (self.stats_good_op.T / all_classes_good_mean).T
+
         else:
             all_classes_good_norm =  self.stats_good_op
 
         sort_ind_tmp = np.argsort(all_classes_good_norm.mean(axis=0))
-        
+
         if self.n_good_perf_ops == None:
             self.stats_good_perf_op_comb  = self.stats_good_op_comb[sort_ind_tmp]
             self.good_perf_op_ids =  self.good_op_ids[sort_ind_tmp]
         else:
-            self.stats_good_perf_op_comb  = self.stats_good_op_comb[sort_ind_tmp][:self.n_good_perf_ops]            
-            self.good_perf_op_ids =  self.good_op_ids[sort_ind_tmp][:self.n_good_perf_ops]            
+            self.stats_good_perf_op_comb  = self.stats_good_op_comb[sort_ind_tmp][:self.n_good_perf_ops]
+            self.good_perf_op_ids =  self.good_op_ids[sort_ind_tmp][:self.n_good_perf_ops]
 
-        
+
 if __name__ == '__main__':
-    
+
     # -----------------------------------------------------------------
     # -- Set Parameters -----------------------------------------------
     # -----------------------------------------------------------------
-    path_pattern = '/home/philip/work/OperationImportanceProject/results/reduced/HCTSA_{:s}_N_70_100_reduced.mat'
+    #path_pattern = '/home/philip/work/OperationImportanceProject/results/reduced/HCTSA_{:s}_N_70_100_reduced.mat'
+    path_pattern = '../input_data/HCTSA_{:s}_N_70_100_reduced.mat'
     path_pattern_task_attrib = "../data/intermediate_results/task_{:s}_{:s}"
-    plot_out_path = '/home/philip/Desktop/tmp/figure_tmp/test.png'
-    result_txt_outpath = '/home/philip/Desktop/tmp/figure_tmp/result_txt.txt'
-    
-    
+    plot_out_path = '../output/figure_tmp/test.png'
+    result_txt_outpath = '../output/figure_tmp/result_txt.txt'
+
     masking_method = 'NaN'
     label_regex_pattern = '.*,(.*)$'
     #task_names = ['Lighting2','OliveOil','FaceFour','N', 'FISH']
-    
-    task_names = ['MedicalImages', 'Cricket_X', 'InlineSkate', 'ECG200', 'WordsSynonyms', 'uWaveGestureLibrary_X', 'Two_Patterns', 'yoga', 'Symbols', 'uWaveGestureLibrary_Z', 'SonyAIBORobotSurfaceII', 'Cricket_Y', 'Gun_Point', 'OliveOil', 'Lighting7', 'NonInvasiveFatalECG _Thorax1', 'Haptics', 'Adiac', 'ChlorineConcentration', 'synthetic_control', 'OSULeaf', 'DiatomSizeReduction', 'SonyAIBORobotSurface', 'MALLAT', 'uWaveGestureLibrary_Y', 'N', 'CBF', 'ECGFiveDays', 'Lighting2', 'FISH', 'FacesUCR', 'FaceFour', 'Trace', 'Coffee', '50words', 'MoteStrain', 'wafer', 'Cricket_Z', 'SwedishLeaf']
 
+#    task_names = ['MedicalImages', 'Cricket_X', 'InlineSkate', 'ECG200', 'WordsSynonyms', 'uWaveGestureLibrary_X', 'Two_Patterns', 'yoga', 'Symbols', 'uWaveGestureLibrary_Z', 'SonyAIBORobotSurfaceII', 'Cricket_Y', 'Gun_Point', 'OliveOil', 'Lighting7', 'NonInvasiveFatalECG _Thorax1', 'Haptics', 'Adiac', 'ChlorineConcentration', 'synthetic_control', 'OSULeaf', 'DiatomSizeReduction', 'SonyAIBORobotSurface', 'MALLAT', 'uWaveGestureLibrary_Y', 'N', 'CBF', 'ECGFiveDays', 'Lighting2', 'FISH', 'FacesUCR', 'FaceFour', 'Trace', 'Coffee', '50words', 'MoteStrain', 'wafer', 'Cricket_Z', 'SwedishLeaf']
+    task_names = ['mouse']
+    
     combine_pair_method = 'mean'
-    combine_tasks_method = 'mean'   
+    combine_tasks_method = 'mean'
     combine_tasks_norm = None
     select_good_perf_ops_norm = 'zscore'
     select_good_perf_ops_method = 'sort_asc'
@@ -231,25 +232,25 @@ if __name__ == '__main__':
     min_calc_tasks = 32
     # -- max distance inside one cluster
     max_dist_cluster = 0.3
-    
+
     # -----------------------------------------------------------------
     # -- Initialise Class instances -----------------------------------
     # -----------------------------------------------------------------
-    
+
     input_method = Data_Input.Datafile_Input(path_pattern,masking_method,label_regex_pattern)
     ranking_method = Feature_Stats.U_Stats(combine_pair_method)
     redundancy_method = Reducing_Redundancy.Reducing_Redundancy(similarity_method = similarity_method,compare_space = compare_space)
-    
+
     workflow = Workflow(task_names,input_method,ranking_method,
                         combine_tasks_method = combine_tasks_method,combine_tasks_norm = combine_tasks_norm,
                         select_good_perf_ops_method = select_good_perf_ops_method, select_good_perf_ops_norm = select_good_perf_ops_norm,
                         redundancy_method = redundancy_method,
                         n_good_perf_ops = n_good_perf_ops)
-    
+
     # -----------------------------------------------------------------
     # -- Do the statistic calculations --------------------------------
-    # -----------------------------------------------------------------    
-    
+    # -----------------------------------------------------------------
+
     # -- calculate the statistics
     if False:
         workflow.read_data()
@@ -258,48 +259,48 @@ if __name__ == '__main__':
     else:
         workflow.read_data(is_read_feature_data = False)
         workflow.load_task_attribute('tot_stats', path_pattern_task_attrib)
-    
+
     # -- find the features which are calculated for at least min_calc_tasks tasks
     workflow.find_good_op_ids(min_calc_tasks)
     # -- Collect all combined stats for each task and take stats for good operations only
     workflow.collect_stats_good_op_ids()
-    # -- Combine the stats of all the tasks 
+    # -- Combine the stats of all the tasks
     workflow.combine_tasks()
     # -- Select a subset of well performing operations
     workflow.select_good_perf_ops()
-    
+
     # -----------------------------------------------------------------
     # -- Do the redundancy calculations -------------------------------
-    # -----------------------------------------------------------------     
-    
+    # -----------------------------------------------------------------
+
     # -- intitialise the redundancy method with the calculated results
     workflow.init_redundancy_method_problem_space()
     # -- calculate the correlation matrix saved in workflow.redundancy_method.similarity_array
     workflow.redundancy_method.calc_similarity()
     # -- calculate the linkage, the cluster indices and the clustering in self.corr_linkage,self.cluster_inds,self.cluster_op_id_list,respectively
     workflow.redundancy_method.calc_hierch_cluster(t = max_dist_cluster)
-    
-   
+
+
     # -----------------------------------------------------------------
     # -- Do the plotting ----------------------------------------------
-    # -----------------------------------------------------------------     
+    # -----------------------------------------------------------------
     # -- initialise the plotting class
     plotting = Plotting.Plotting(workflow,max_dist_cluster = max_dist_cluster)
-    
+
     if False:
         # -- Plot the statistics array
         plotting.plot_stat_array()
     else:
-        # -- Plot the similarity array   
+        # -- Plot the similarity array
         plotting.plot_similarity_array()
-    
+
     plt.savefig(plot_out_path)
 
-    
-    
+
+
     # -----------------------------------------------------------------
     # -- Output the results to text file-------------------------------
-    # -----------------------------------------------------------------       
+    # -----------------------------------------------------------------
     op_id_name_map = plotting.map_op_id_name_mult_task(workflow.tasks)
     # -- write not reduced top performing features indicating the respective clusters they belong to
     # -- number of problems for which each good performing feature has been calculated
@@ -310,14 +311,14 @@ if __name__ == '__main__':
     measures[1,:] = (~workflow.stats_good_op.mask).sum(axis=0)
     # -- z scored u-stat
     measures[2,:] = fap.normalise_masked_array(workflow.stats_good_op_comb, axis= 0,norm_type = 'zscore')[0]
-   
-    # -- write textfile containing the information as shown in the plot     
-    workflow.redundancy_method.write_cluster_file(result_txt_outpath,op_id_name_map,measures) 
-    
-    
-    # -----------------------------------------------------------------    
+
+    # -- write textfile containing the information as shown in the plot
+    workflow.redundancy_method.write_cluster_file(result_txt_outpath,op_id_name_map,measures)
+
+
+    # -----------------------------------------------------------------
     # -- show the plot as last task of the script
-    # -----------------------------------------------------------------    
+    # -----------------------------------------------------------------
     plt.show()
 
     # -- write not reduced top performing features to text file
@@ -326,11 +327,3 @@ if __name__ == '__main__':
 #                                       hlp.ind_map_subset(op_id_name_map[0],op_id_name_map[1], workflow.good_perf_op_ids),
 #                                       workflow.stats_good_perf_op_comb):
 #             out_result_txt_file.write("{:d} {:s} {:f}\n".format(op_id,op_name,op_U))
-
-    
-    
-    
-    
-    
-    
-    

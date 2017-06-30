32b0587874d9df41f26fa928159b89a481a3e0ad
Sarab Sethi <s.sethi16@imperial.ac.uk>
2017-06-30 17:20:52 +0100

    new download data matlab script

--- op_importance/input_data/download_data_from_db.m
+++ op_importance/input_data/download_data_from_db.m
@@ -1,32 +1,27 @@
 clear variables;
 
-dbc = SQL_opendatabase;
-
-
-SQL_retrieve(1:50,'all','all')
-
-SQL_closedatabase(dbc);
-
-TS_normalize()
-delete('HCTSA.mat')
-
-clear variables
-load('HCTSA_N.mat')
-save('HCTSA_ts1_N_70_100_reduced.mat','-v7')
-delete('HCTSA_N.mat')
-
-clear variables;
+task_splits = [1,905;906,1686;1687,1746;1747,1802;1,5];
+task_names = {'50_words','Adiac','Beef','Coffee','test'};
 
 dbc = SQL_opendatabase;
 
-SQL_retrieve(1000:1050,'all','all')
+for i = 5:length(task_names)
+    % Download data
+    SQL_retrieve(task_splits(i,1):task_splits(i,2),'all','all')
+    % Normalise data
+    
+    try
+        TS_normalize();
+        delete('HCTSA.mat');
+        % Save normalised data in -v7 .mat format
+        norm_data = load('HCTSA_N.mat');
+        fname = sprintf('HCTSA_%s_N_70_100_reduced.mat',task_names{i});
+        save(fname,'-struct','norm_data','-v7');
+        delete('HCTSA_N.mat')
+    catch
+        warning('Error whilst processing data for %s',task_names{i});
+    end
+    
+end
 
 SQL_closedatabase(dbc);
-
-TS_normalize()
-delete('HCTSA.mat')
-
-clear variables
-load('HCTSA_N.mat')
-save('HCTSA_ts2_N_70_100_reduced.mat','-v7')
-delete('HCTSA_N.mat')

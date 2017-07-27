142bba7b8cc6bc67b0920f592692da41c1f41a77
sarabsethi <sarab.sethi93@gmail.com>
2017-07-27 16:13:03 +0100

    HPC job batch submitting code. parameterise tasks in Workflow.py

---
+++ op_importance/hpc_job_scripts/submit_workflow_jobs.sh
@@ -0,0 +1,30 @@
+## interpreter directive - this is a bash script
+#!/bin/bash
+
+timePerJob=1
+cpusPerJob=4
+memPerJob=8
+
+## declare an array variable
+declare -a runtypes=("svm_maxmin" "svm_maxmin_null")
+
+## declare an array variable
+declare -a tasks=("50words" "Adiac" "ArrowHead" "Beef" "BeetleFly" "BirdChicken" "CBF" "Car""ChlorineConcentration" "CinC_ECG_torso" "Coffee" "Computers" "Cricket_X" "Cricket_Y""Cricket_Z" "DiatomSizeReduction" "DistalPhalanxOutlineAgeGroup" "DistalPhalanxOutlineCorrect""DistalPhalanxTW" "ECG200" "ECG5000" "ECGFiveDays" "Earthquakes" "ElectricDevices" "FISH" "FaceAll" "FaceFour" "FacesUCR" "FordA" "FordB" "Gun_Point" "Ham" "HandOutlines""Haptics" "Herring" "InlineSkate" "InsectWingbeatSound" "ItalyPowerDemand""LargeKitchenAppliances" "Lighting2" "Lighting7" "MALLAT" "Meat" "MedicalImages""MiddlePhalanxOutlineAgeGroup" "MiddlePhalanxOutlineCorrect" "MiddlePhalanxTW" "MoteStrain""NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "OSULeaf" "OliveOil""PhalangesOutlinesCorrect" "Phoneme" "Plane" "ProximalPhalanxOutlineAgeGroup""ProximalPhalanxOutlineCorrect" "ProximalPhalanxTW" "RefrigerationDevices" "ScreenType""ShapeletSim" "ShapesAll" "SmallKitchenAppliances" "SonyAIBORobotSurface""SonyAIBORobotSurfaceII" "StarLightCurves" "Strawberry" "SwedishLeaf" "Symbols""ToeSegmentation1" "ToeSegmentation2" "Trace" "TwoLeadECG" "Two_Patterns""UWaveGestureLibraryAll" "Wine" "WordsSynonyms" "Worms" "WormsTwoClass" "synthetic_control""uWaveGestureLibrary_X" "uWaveGestureLibrary_Y" "uWaveGestureLibrary_Z" "wafer" "yoga")
+
+submitFolder="submit_scripts"
+mkdir $submitFolder
+
+## first loop through the runtypes
+for rtype in "${runtypes[@]}"
+do
+  ## now loop through the tasks
+  for i in "${tasks[@]}"
+  do
+    ScriptLocation="submit_scripts/job-$i-$rtype.sh"
+    sed -e "s/xxxTIMEHRSxxx/$timePerJob/g" -e "s/xxxCPUSxxx/$cpusPerJob/g" -e "s/xxxRAMGBxxx/$memPerJob/g" -e "s/xxxRUNTYPExxx/$rtype/g" -e "s/xxxTASKSxxx/$i/g" workflow_job_template.sh > $ScriptLocation
+    qsub $ScriptLocation
+    echo "Sumbmitted job $ScriptLocation"
+  done
+done
+
+rm -r $submitFolder
---
+++ op_importance/hpc_job_scripts/workflow_job_template.sh
@@ -0,0 +1,13 @@
+## interpreter directive - this is a shell script
+#!/bin/sh
+## ask PBS for time (format hh:mm:ss)
+#PBS -l walltime=xxxTIMEHRSxxx:00:00
+## ask for one node with some cpus and memory (per node)
+#PBS -l select=1:ncpus=xxxCPUSxxx:mem=xxxRAMGBxxxgb
+
+##load anaconda python module
+module purge module load anaconda3/personal
+
+##command line
+cd ~/op_importance/op_importance/workflow_classes/
+/home/ss7412/anaconda3/envs/py27/bin/python Workflow.py "xxxRUNTYPExxx" "xxxTASKSxxx"

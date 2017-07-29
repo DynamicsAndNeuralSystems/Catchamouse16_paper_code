de351f308550f7004c9d82d4a14112ffbf91e402
sarabsethi <sarab.sethi93@gmail.com>
2017-07-29 22:15:22 +0100

    param for super long tasks

--- op_importance/hpc_job_scripts/submit_workflow_jobs.sh
+++ op_importance/hpc_job_scripts/submit_workflow_jobs.sh
@@ -11,6 +11,7 @@ declare -a runtypes=("svm_maxmin" "svm_maxmin_null" "dectree_maxmin" "dectree_ma
 declare -a tasks=("50words" "Adiac" "ArrowHead" "Beef" "BeetleFly" "BirdChicken" "CBF" "Car" "ChlorineConcentration" "CinC_ECG_torso" "Coffee" "Computers" "Cricket_X" "Cricket_Y" "Cricket_Z" "DiatomSizeReduction" "DistalPhalanxOutlineAgeGroup" "DistalPhalanxOutlineCorrect" "DistalPhalanxTW" "ECG200" "ECG5000" "ECGFiveDays" "Earthquakes" "ElectricDevices" "FISH" "FaceAll" "FaceFour" "FacesUCR" "FordA" "FordB" "Gun_Point" "Ham" "HandOutlines" "Haptics" "Herring" "InlineSkate" "InsectWingbeatSound" "ItalyPowerDemand" "LargeKitchenAppliances" "Lighting2" "Lighting7" "MALLAT" "Meat" "MedicalImages" "MiddlePhalanxOutlineAgeGroup" "MiddlePhalanxOutlineCorrect" "MiddlePhalanxTW" "MoteStrain" "NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "OSULeaf" "OliveOil" "PhalangesOutlinesCorrect" "Phoneme" "Plane" "ProximalPhalanxOutlineAgeGroup" "ProximalPhalanxOutlineCorrect" "ProximalPhalanxTW" "RefrigerationDevices" "ScreenType" "ShapeletSim" "ShapesAll" "SmallKitchenAppliances" "SonyAIBORobotSurface" "SonyAIBORobotSurfaceII" "StarLightCurves" "Strawberry" "SwedishLeaf" "Symbols" "ToeSegmentation1" "ToeSegmentation2" "Trace" "TwoLeadECG" "Two_Patterns" "UWaveGestureLibraryAll" "Wine" "WordsSynonyms" "Worms" "WormsTwoClass" "synthetic_control" "uWaveGestureLibrary_X" "uWaveGestureLibrary_Y" "uWaveGestureLibrary_Z" "wafer" "yoga")
 
 declare -a longerTasks=("ChlorineConcentration" "ECG5000" "ElectricDevices" "FordA" "FordB" "NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "StarLightCurves" "Two_Patterns" "UWaveGestureLibraryAll" "uWaveGestureLibrary_X" "uWaveGestureLibrary_Y" "uWaveGestureLibrary_Z" "wafer")
+declare -a longestTasks=("ElectricDevices" "StarLightCurves")
 
 submitFolder="submit_scripts"
 mkdir $submitFolder
@@ -27,6 +28,9 @@ do
       timePerJob=2:00:00
       if [[ " ${longerTasks[@]} " =~ " ${i} " ]]; then
         timePerJob=5:00:00
+      fi
+      if [[ " ${longestTasks[@]} " =~ " ${i} " ]]; then
+        timePerJob=8:00:00
       fi
     fi
 

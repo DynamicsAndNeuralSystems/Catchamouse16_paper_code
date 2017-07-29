02d698838208a97652493e23039a74f1fd005e74
sarabsethi <sarab.sethi93@gmail.com>
2017-07-29 16:39:29 +0100

    add more tasks which should be given longer

--- op_importance/hpc_job_scripts/submit_workflow_jobs.sh
+++ op_importance/hpc_job_scripts/submit_workflow_jobs.sh
@@ -10,7 +10,7 @@ declare -a runtypes=("svm_maxmin" "svm_maxmin_null" "dectree_maxmin" "dectree_ma
 ## declare an array variable
 declare -a tasks=("50words" "Adiac" "ArrowHead" "Beef" "BeetleFly" "BirdChicken" "CBF" "Car" "ChlorineConcentration" "CinC_ECG_torso" "Coffee" "Computers" "Cricket_X" "Cricket_Y" "Cricket_Z" "DiatomSizeReduction" "DistalPhalanxOutlineAgeGroup" "DistalPhalanxOutlineCorrect" "DistalPhalanxTW" "ECG200" "ECG5000" "ECGFiveDays" "Earthquakes" "ElectricDevices" "FISH" "FaceAll" "FaceFour" "FacesUCR" "FordA" "FordB" "Gun_Point" "Ham" "HandOutlines" "Haptics" "Herring" "InlineSkate" "InsectWingbeatSound" "ItalyPowerDemand" "LargeKitchenAppliances" "Lighting2" "Lighting7" "MALLAT" "Meat" "MedicalImages" "MiddlePhalanxOutlineAgeGroup" "MiddlePhalanxOutlineCorrect" "MiddlePhalanxTW" "MoteStrain" "NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "OSULeaf" "OliveOil" "PhalangesOutlinesCorrect" "Phoneme" "Plane" "ProximalPhalanxOutlineAgeGroup" "ProximalPhalanxOutlineCorrect" "ProximalPhalanxTW" "RefrigerationDevices" "ScreenType" "ShapeletSim" "ShapesAll" "SmallKitchenAppliances" "SonyAIBORobotSurface" "SonyAIBORobotSurfaceII" "StarLightCurves" "Strawberry" "SwedishLeaf" "Symbols" "ToeSegmentation1" "ToeSegmentation2" "Trace" "TwoLeadECG" "Two_Patterns" "UWaveGestureLibraryAll" "Wine" "WordsSynonyms" "Worms" "WormsTwoClass" "synthetic_control" "uWaveGestureLibrary_X" "uWaveGestureLibrary_Y" "uWaveGestureLibrary_Z" "wafer" "yoga")
 
-declare -a longerTasks=("ECG5000" "ElectricDevices" "FordA" "NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "StarLightCurves" "Two_Patterns" "wafer")
+declare -a longerTasks=("ChlorineConcentration" "ECG5000" "ElectricDevices" "FordA" "FordB" "NonInvasiveFatalECG_Thorax1" "NonInvasiveFatalECG_Thorax2" "StarLightCurves" "Two_Patterns" "UWaveGestureLibraryAll" "uWaveGestureLibrary_X" "uWaveGestureLibrary_Y" "uWaveGestureLibrary_Z" "wafer")
 
 submitFolder="submit_scripts"
 mkdir $submitFolder
@@ -24,10 +24,8 @@ do
     # dectree takes longer. some take especially long...
     timePerJob=1:00:00
     if [[ $rtype == *"svm"* ]]; then
-      echo "svm"
       timePerJob=2:00:00
       if [[ " ${longerTasks[@]} " =~ " ${i} " ]]; then
-        echo 'longerTasks';
         timePerJob=5:00:00
       fi
     fi

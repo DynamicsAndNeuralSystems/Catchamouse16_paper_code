3dfb92f7a5def994ce118dc89c98f596892c914a
Sarab Sethi <s.sethi16@imperial.ac.uk>
2017-08-07 12:18:40 +0100

    plot missing time series post norm

--- op_importance/sarab_matlab_analysis/check_ts_normalisation.m
+++ op_importance/sarab_matlab_analysis/check_ts_normalisation.m
@@ -7,6 +7,7 @@ mat_files = dir('./HCTSA_*.mat');
 i_task = 1;
 ts_missing = {};
 task_names = {};
+ts_missing_data = {};
 
 for i = 1:length(mat_files)
     f = mat_files(i).name;
@@ -24,7 +25,16 @@ for i = 1:length(mat_files)
     norm_ids = [norm.TimeSeries.ID];
     
     missing = setdiff(orig_ids,norm_ids);
+    missing_idxs = find(ismember(orig_ids,missing));
+    
     if ~isempty(missing)
+        figure;
+        for j = 1:length(missing_idxs)
+            ts_data = orig.TimeSeries(missing_idxs(j)).Data;
+            plot(ts_data);
+            hold on;
+            ts_missing_data{end+1} = ts_data;
+        end
         fraction = num2str((length(missing) / length(orig_ids))*100);
         fprintf([int2str(length(missing)),' (',fraction,'%%) time series removed after normalisation from ',core_name,'\n']);
     end
--- op_importance/sarab_matlab_analysis/look_at_dectree_p_vals.m
+++ op_importance/sarab_matlab_analysis/look_at_dectree_p_vals.m
@@ -1,15 +1,14 @@
 clear variables;
 
 nulls_folder = ['C:/Users/Sarab/Documents/op_importance_data/hpc_individual_nulls/intermediate_results_dectree_maxmin_null/'];
-if ~exist(nulls_folder)
-    nulls_folder = '../data/intermediate_results_dectree_maxmin_null/';
-end
+nulls_folder = '../data/intermediate_results_dectree_maxmin_null/';
+
 
 task_names = {'50words','Adiac','ArrowHead','Beef','BeetleFly','BirdChicken','CBF','Car','ChlorineConcentration','CinC_ECG_torso','Coffee','Computers','Cricket_X','Cricket_Y','Cricket_Z','DiatomSizeReduction','DistalPhalanxOutlineAgeGroup','DistalPhalanxOutlineCorrect','DistalPhalanxTW','ECG200','ECG5000','ECGFiveDays','Earthquakes','ElectricDevices','FISH','FaceAll','FaceFour','FacesUCR','FordA','FordB','Gun_Point','Ham','HandOutlines','Haptics','Herring','InlineSkate','InsectWingbeatSound','ItalyPowerDemand','LargeKitchenAppliances','Lighting2','Lighting7','MALLAT','Meat','MedicalImages','MiddlePhalanxOutlineAgeGroup','MiddlePhalanxOutlineCorrect','MiddlePhalanxTW','MoteStrain','NonInvasiveFatalECG_Thorax1','NonInvasiveFatalECG_Thorax2','OSULeaf','OliveOil','PhalangesOutlinesCorrect','Phoneme','Plane','ProximalPhalanxOutlineAgeGroup','ProximalPhalanxOutlineCorrect','ProximalPhalanxTW','RefrigerationDevices','ScreenType','ShapeletSim','ShapesAll','SmallKitchenAppliances','SonyAIBORobotSurface','SonyAIBORobotSurfaceII','StarLightCurves','Strawberry','SwedishLeaf','Symbols','ToeSegmentation1','ToeSegmentation2','Trace','TwoLeadECG','Two_Patterns','UWaveGestureLibraryAll','Wine','WordsSynonyms','Worms','WormsTwoClass','synthetic_control','uWaveGestureLibrary_X','uWaveGestureLibrary_Y','uWaveGestureLibrary_Z','wafer','yoga'};
 task_names = {'WordsSynonyms'};
 
 max_tasks = 1;
-max_ops = Inf;
+max_ops = 100;
 op_pdfs = {};
 op_pdfs_xi = {};
 
